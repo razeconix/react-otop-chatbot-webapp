a29a86b7d3eed039ae1d1e44db837eb7a9b9ddc6
Kiratiphan Phanmak <kiratiphanp60@email.nu.ac.th>
2020-08-30 11:52:40 +0700

    Complete shoplist and product detail complate fixed card render

--- client/src/components/Chatbot/Card.js
+++ client/src/components/Chatbot/Card.js
@@ -5,24 +5,20 @@ const Card = (props) => {
     return (
         <div  style={{ height: 270, paddingRight:30, float: 'left'}}>
             <div className="card">
-                {/*---------------------------------*/}
-                        <div className="card-image" style={{ width: 240}}>
-                    <       img alt={props.payload.fields.header.stringValue} src={props.payload.fields.image.stringValue} />  
-                        </div>
-                {/*---------------------------------*/}
-                        <div className="card-content">
-                            <span className="card-title">{props.payload.fields.header.stringValue}</span>
-                                {props.payload.fields.description.stringValue}
-                            <p>{props.payload.fields.price.stringValue}</p>
-                         </div>
-                {/*---------------------------------*/}
-                            <div className="card-action">
-                             <a target="_blank" rel="noopener noreferrer" href={props.payload.fields.link.stringValue}>ดูข้อมูลเพิ่มเติม</a>
-                            </div>
-                {/*---------------------------------*/}
+                <div className="card-image" style={{ width: 160}}>
+                    <img alt={props.payload.fields.header.stringValue} src={props.payload.fields.image.stringValue} />
+                </div>
+                <div className="card-content" style={{ height: 100}}>
+                <p className="card-title">{props.payload.fields.header.stringValue}</p>
+                    <p> <a href="/">{props.payload.fields.price.stringValue}</a></p>
+                </div>
+                <div className="card-action">
+                    <a target="_blank" rel="noopener noreferrer" href={props.payload.fields.link.stringValue}>ดูข้อมูลเพิ่มเติม</a>
+                </div>
             </div>
         </div>
     );
 };
 
 export default Card; 
+
