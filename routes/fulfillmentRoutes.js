60fb80311f5f2362ab9fc12e47ee0a44eddba102
Kiratiphan Phanmak <kiratiphanp60@email.nu.ac.th>
2020-09-21 19:44:04 +0700

    add fulfillment

--- routes/fulfillmentRoutes.js
+++ routes/fulfillmentRoutes.js
@@ -6,20 +6,22 @@ const Product = require('../models/Product');
 module.exports = app => {
     app.post('/', async (req, res) => {
         const agent = new WebhookClient({ request: req, response: res });
-
+       
+        
+        
         async function getProduct(agent) {
+        
             
             let product = await Product.findOne({'product_name' : agent.parameters.product_name});
             if (product !== null ) {
                 responseText = `${agent.parameters.product_name} ราคา ${product.price} บาท`;
             }
-
-            agent.add(responseText);
+            agent.add(responseText)
         
         }
 
         function fallback(agent) {
-            agent.add(`I didn't understand`);
+            agent.add(`ขอโทษนะคะ ไม่เจอข้อมูลสินค้า`);
             agent.add(`I'm sorry, can you try again?`);
         }
         let intentMap = new Map();
