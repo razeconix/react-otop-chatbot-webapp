caf0eea7a1d61d7ec351c5efae92ec2100462474
Kiratiphan Phanmak <kiratiphanp60@email.nu.ac.th>
2020-10-05 21:23:06 +0700

    add cloudinary function to upload image

--- client/src/components/Chatbot/Card.js
+++ client/src/components/Chatbot/Card.js
@@ -7,10 +7,12 @@ const Card = (props) => {
             <div className="card">
                 <div className="card-image" style={{ width: 240}}>
                     <img alt={props.payload.fields.header.stringValue} style={{height:180}} src={props.payload.fields.image.stringValue} />
+                    
                 </div>
                 <div className="card-content">
-                <p className="card-title">{props.payload.fields.header.stringValue}</p>
-                    <p> <a href="">{props.payload.fields.price.stringValue}</a></p>
+                    <p>{props.payload.fields.header.stringValue}</p>
+                    <p> หมวดหมู่ : {props.payload.fields.category.stringValue}</p>
+                    <p> ราคา : {props.payload.fields.price.stringValue}</p>
                 </div>
                 <div className="card-action">
                     <a target="_blank" rel="noopener noreferrer" href={props.payload.fields.link.stringValue}>ดูข้อมูลเพิ่มเติม</a>
